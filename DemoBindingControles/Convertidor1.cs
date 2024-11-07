8649e88b5a2451bb03c73601bd7969566c8b0390
Madrigal <m.madrigallosa@edu.gva.es>
2024-11-07 12:44:45 +0100

    Agregar archivos de proyecto.

---
+++ DemoBindingControles/Convertidor1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DemoBindingControles
+{
+    class Convertidor1 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var cadena = value as string;
+            switch (cadena.ToString().ToLower()) {
+                case "yes":
+                case "si":
+                case "oui":
+                    return true;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            MessageBox.Show("No pulses el checkbox con el ratón");
+            return "Error...";
+
+        }
+    }
+}
---
+++ DemoBindingControles/Convertidor2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DemoBindingControles
+{
+    class Convertidor2 : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var activado = value as Boolean?;
+            if (activado == true)
+                return "Visible";
+            else
+                return "Hidden";
+
+            /*
+             return (bool)value?"Visible","Hidden";
+             */
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
